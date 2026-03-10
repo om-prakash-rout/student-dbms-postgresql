bb43b765dd001e24df6ea898b9c7db0f260bf322
om-prakash-rout <om.prakash.rout720@gmail.com>
2026-03-10 11:15:07 +0530

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,15 +1,31 @@
--- Create students table
+-- Students table
 CREATE TABLE students (
-    student_id SERIAL PRIMARY KEY,
+    student_id VARCHAR(20) PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     course VARCHAR(50),
+    semester INT,
+    email VARCHAR(100),
+    phone VARCHAR(15)
+);
+
+-- Subjects table
+CREATE TABLE subjects (
+    subject_id SERIAL PRIMARY KEY,
+    subject_name VARCHAR(50),
     semester INT
 );
 
--- Create marks table
+-- Marks table
 CREATE TABLE marks (
     mark_id SERIAL PRIMARY KEY,
-    student_id INT REFERENCES students(student_id),
-    subject VARCHAR(50),
+    student_id VARCHAR(20) REFERENCES students(student_>
+    subject_id INT REFERENCES subjects(subject_id),
     marks INT
+);
+
+-- Attendance table
+CREATE TABLE attendance (
+    attendance_id SERIAL PRIMARY KEY,
+    student_id VARCHAR(20) REFERENCES students(student_>
+    attendance_percent INT
 );
